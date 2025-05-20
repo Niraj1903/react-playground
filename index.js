89d4fe6eebd0fa507e7dfc1e49edb454eb6363e7
Niraj Singh <niraj.singh1903@outlook.com>
2025-05-20 21:45:32 +0530

    20th May 2025

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const h1 = document.createElement("h1");
 
-h1.textContent = "Hello world from React-playground";
+h1.textContent = "Hello world from React";
 document.getElementById("root").appendChild(h1);
