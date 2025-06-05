0602d73c416fd5e9bb9293118cbe012ef9221870
Niraj Singh <niraj.singh1903@outlook.com>
2025-06-05 23:16:11 +0530

    05 June 2025

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./Header";
-import Search from "./Body/Search";
-import Resturant from "./Body/Resturant";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Resturant from "./components/Resturant";
 
 export default function App() {
   return (
