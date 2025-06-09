e3b01101e06710f1971b3c237f00e23a1fddc865
Niraj Singh <niraj.singh1903@outlook.com>
2025-06-09 23:20:34 +0530

    swiggy api issue resolved

--- src/components/Resturant.js
+++ src/components/Resturant.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
 
 export default function Resturant() {
-  return (
+  const [resturantCard, setResturantCard] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8467244&lng=77.6431526&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const response = await data.json();
+    // console.log(response);
+    setResturantCard(
+      response?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  return resturantCard === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="res-card">
-        <h3>Meghana Foods</h3>
+        {resturantCard.map((item) => (
+          <p>{item.info.name}</p>
+        ))}
       </div>
     </>
   );
