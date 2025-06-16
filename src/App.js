eee0b61767191e7f148033aea9d093e85e806bcf
bhadresharya <bhadresharya@hotmail.com>
2025-06-16 17:58:19 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Resturant from "./components/ResturantCard";
 import Body from "./components/Body";
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+);
 
 export default function App() {
   return (
     <>
       <div className="app">
         <Header />
-        <Body />
+        <Router>
+          <Body path="/" />
+          <About path="/about" />
+        </Router>
       </div>
     </>
   );
