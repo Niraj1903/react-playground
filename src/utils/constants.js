1ddfce5b1ed2cfe93dc5e6ebe31f7a5e78a58916
Niraj1903 <niraj.singh1903@outlook.com>
2025-07-01 22:59:56 +0530

    CORS issue fixed-1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,7 @@ const RESTURANT_MENU_IMG_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
 
 const RESTURANT_MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8467244&lng=77.6431526&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8467244&lng=77.6431526&restaurantId=";
 
 const RESTURANT_API =
   "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8467244&lng=77.6431526&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
